6c44aa115122aeca63ce8389daee742444144cee
WebMobik <ddimon3000@bk.ru>
2020-02-03 19:56:58 +0300

    Add a shop data and create shop component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={Homepage} />
+      <Route path='/shop-page' component={ShopPage}/>
     </div>
   );
 }
