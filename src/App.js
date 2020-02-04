6cb5f63d81a75601612c1fc5724ca35fe75b1726
WebMobik <ddimon3000@bk.ru>
2020-02-04 09:04:28 +0300

    Create header component and added styles for it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop-page' component={ShopPage}/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
