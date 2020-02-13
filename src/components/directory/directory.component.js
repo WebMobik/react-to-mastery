81251353810c7cb87c0b006c26dbe2758d26e483
WebMobik <ddimon3000@bk.ru>
2020-02-13 21:13:05 +0300

    Directory and Shop into Redux

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,56 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './directory.styles.scss';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import MenuItem from '../menu-item/menu-item.component';
 
-export default class Directory extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-                    id: 1,
-                    linkUrl: '',
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-                    id: 2,
-                    linkUrl: '',
-                },
-                {
-                    title: 'snearers',
-                    imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-                    id: 3,
-                    linkUrl: '',
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-                    id: 4,
-                    linkUrl: '',
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-                    id: 5,
-                    linkUrl: '',
-                },
-            ]
+const Directory = ({ sections }) => (
+    <div className="directory-menu">
+        {
+            sections.map(({ id, ...props }) => (
+                <MenuItem key={id} { ...props } />
+            ))
         }
-    }
-    render() {
-        return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({ id, ...props }) => (
-                        <MenuItem key={id} { ...props } />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
+
+export default connect(mapStateToProps)(Directory);
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector} from 'reselect';
 
-import SHOP_DATA from './shop.data';
+import { selectCollections } from '../../redux/shop/shop.selector';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-export default class ShopPage extends Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        {
+            collections.map(({ id, ...props }) => (
+                <CollectionPreview key={id} {...props} />
+            ))
+        }
+    </div>
+)
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...props }) => (
-                        <CollectionPreview key={id} {...props} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+export default connect(mapStateToProps)(ShopPage);
